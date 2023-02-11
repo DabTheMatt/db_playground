08eb1ee64319c71040134766a0ecb550e1afee7e
DabTheMatt <inspiromaciek@gmail.com>
2023-02-11 13:06:37 +0100

    connecting firebase

--- db-notes/src/App.js
+++ db-notes/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { db } from './firebase';
+import { collection, getDocs } from 'firebase/firestore';
 
 function App() {
+
+const [users, setUsers] = useState([]);
+const usersRef = collection(db, 'users');
+
+useEffect(() => {
+  const getUsers = async () => {
+    const data = await getDocs(usersRef);
+    console.log(data);
+    setUsers(data.docs);
+    // setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+  };
+
+  getUsers();
+}, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      {users.map((user) => {
+        return (
+          <div>
+            <h1>Name: {user.name}</h1>
+            <h2>Age: {user.age}</h2>
+          </div>
+        )
+      })}
     </div>
-  );
+  )
 }
 
 export default App;
